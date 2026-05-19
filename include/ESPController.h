0a2506521ca145f2a782edbcaaa25a7b6917f814
eliaswasem <ewasem@outlook.de>
2026-05-19 21:03:06 +0200

    structure for ESP Communication

---
+++ include/ESPController.h
@@ -0,0 +1,18 @@
+//
+// Created by elias on 19.05.26.
+//
+
+#pragma once
+
+#include <Arduino.h>
+
+class ESPController {
+
+
+public:
+    void begin();
+    void update();
+
+};
+
+#endif // ESP_CONTROLLER_H
